bfde321e7bfd60d6c2786a98cbc3813fade47c8b
clice lee <cliceleee@gmail.com>
2024-11-12 21:50:40 +0900

    design: add git face, Noto sans font

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import "./index.css";
 
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen min-w-full bg-white">
+    <div className="flex items-center justify-center min-h-screen min-w-full bg-white font-bold">
       <div className="flex flex-row justify-between p-4 h-[90%] w-[90%] max-w-5xl">
         <div className="flex flex-col items-center justify-center w-1/2">
           <Face />
--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const Face = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="text-9xl">😊</div>
+      <div className="text-9xl">
+        <img src="/assets/smile.gif" alt="Smiling face" />
+      </div>
     </div>
   );
 };
