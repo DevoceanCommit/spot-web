8efa9648e910ddcd8a4463fb0617c32c5660bcaf
clice lee <cliceleee@gmail.com>
2024-11-15 01:52:38 +0900

    feat: get sensor data from the server

--- src/components/SensorData.jsx
+++ src/components/SensorData.jsx
@@ -8,23 +8,30 @@ const SensorData = () => {
     movement: "",
     gas: "",
     sound: "",
+    sound_in: "",
   });
   const [error, setError] = useState(null);
 
   const fetchSensorData = async () => {
     try {
-      const response = await axios.get("/api/sensor");
-      const data = response.data;
+      const response = await axios.get(
+        "http://203.250.148.52:48003/api/sensor"
+      );
+      const latestData = response.data[0].data; // 가장 최신 데이터 (배열의 첫 번째 항목)
+
       setSensorData({
-        temperature: data.temperature,
-        humidity: data.humidity,
-        movement: data.movement,
-        gas: data.gas,
-        sound: data.sound,
+        temperature: latestData.temperature || { out: "-", in: "-" },
+        humidity: latestData.humidty || { out: "-", in: "-" }, // 오타 'humidty'에 주의
+        movement: latestData.movement || "-",
+        gas: latestData.gas || "-",
+        sound: latestData.sound || "-",
+        sound_in: latestData.sound_in || "-",
       });
+
+      console.log("성공", latestData);
     } catch (error) {
       console.error("센서 데이터 가져오기 오류:", error);
-      setError("안녕하세요, 오늘도 좋은 하루 되세요"); // 화면에 표시될 에러 메시지
+      setError("센서 데이터를 불러올 수 없습니다.");
     }
   };
 
@@ -56,8 +63,12 @@ const SensorData = () => {
             <div>{sensorData.gas}</div>
           </div>
           <div>
-            <div>소음</div>
+            <div>소음 상태</div>
             <div>{sensorData.sound}</div>
+          </div>
+          <div>
+            <div>소음 레벨 (실내)</div>
+            <div>{sensorData.sound_in}</div>
           </div>
         </>
       )}
